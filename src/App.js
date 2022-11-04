0c1d337c4ace9f8de3f07d549f4cf36e5e1f0ecd
bonieck <bonieck.douglas@hotmail.com>
2022-11-04 19:29:30 -0300

    originais netflix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import tmdb from './tmdb';
 import Movierow from './components/Movierow';
+import FeaturedMovie from "./components/FeaturedMovie";
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect (()=> {
         const loadAll = async () => {
             //Pegando a lista total
             let list = await tmdb.getHomeList();
             setMovieList(list);
+
+            
+
         }
         loadAll();
     }, []);
@@ -20,6 +25,9 @@ export default () => {
 
     return(
         <div className="page">
+            {featuredData &&
+            <FeaturedMovie item = {featuredData}/>
+            }
             <section className="lists">
                 {movieList.map((item, key) => (
                     <Movierow key={key} title={item.title} items={item.items} alt={item.original_title}/>
