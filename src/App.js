4d707118aaf90fff9e88c2be3798242f2ab7739a
bonieck <bonieck.douglas@hotmail.com>
2022-11-05 00:02:53 -0300

    header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import tmdb from './tmdb';
 import Movierow from './components/Movierow';
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
     const [featuredData, setFeaturedData] = useState(null);
+    const [blackHeader, setBlackHeader] = useState(false);
 
     useEffect (()=> {
         const loadAll = async () => {
@@ -15,16 +17,42 @@ export default () => {
             let list = await tmdb.getHomeList();
             setMovieList(list);
 
-            
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeaturedData(chosenInfo);
 
         }
         loadAll();
     }, []);
 
 
+    useEffect(() =>{
+        const scrollListener = () => {
+            if(window.scrollY > 10){
+                setBlackHeader(true);
+            } else{
+                setBlackHeader(false);
+            }
+
+        }
+
+        window.addEventListener('scroll', scrollListener);
+
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+
+    }, []);
+
+
 
     return(
         <div className="page">
+            
+            <Header black={blackHeader} />
+
             {featuredData &&
             <FeaturedMovie item = {featuredData}/>
             }
